340e4409e84f3f2f79bb3bb9f4f9987d33dda297
Proc3ssa <pros3sa@gmail.com>
2026-06-15 21:41:00 +0000

    config: add app router

--- routes/AppRouter.jsx
+++ routes/AppRouter.jsx
@@ -1,11 +1,32 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "../pages/auth/Login";
+import PrivateRoute from "../src/routes/PrivateRoute";
+import OrganizerDashboard from "../src/pages/dashboard/OrganizerDashboard";
+import ReceptionistDashboard from "../src/pages/dashboard/ReceptionistDashboard";
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
+
+        <Route
+          path="/dashboard"
+          element={
+            <PrivateRoute allowedRoles={["organizer"]}>
+              <OrganizerDashboard />
+            </PrivateRoute>
+          }
+        />
+
+        <Route
+          path="/scan"
+          element={
+            <PrivateRoute allowedRoles={["receptionist"]}>
+              <ReceptionistDashboard />
+            </PrivateRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
