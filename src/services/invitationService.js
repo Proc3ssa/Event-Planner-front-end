1498adc9aaf1da948409a4fe721e6d29ffa59b1f
Proc3ssa <pros3sa@gmail.com>
2026-06-26 18:20:03 +0000

    update: add new functions to invite service

--- src/services/invitationService.js
+++ src/services/invitationService.js
@@ -14,3 +14,23 @@ export const respondToInvitation = async (token, response) => {
   const { data } = await api.patch(`/api/invitations/${token}/respond`, { response });
   return data;
 };
+
+export const getEventInvitations = async (event_id) => {
+  const { data } = await api.get(`/api/invitations/event/${event_id}`);
+  return data.invitations;
+};
+
+export const setAttendance = async (id, attendance) => {
+  const { data } = await api.patch(`/api/invitations/${id}/attendance`, { attendance });
+  return data;
+};
+
+export const setTableNumber = async (id, table_number) => {
+  const { data } = await api.patch(`/api/invitations/${id}/table`, { table_number });
+  return data;
+};
+
+export const removeInvitation = async (id) => {
+  const { data } = await api.delete(`/api/invitations/${id}`);
+  return data;
+};
